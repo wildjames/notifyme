43ad8c1f210988025ce933da4b2d49f6a910bfee
wildjames <jwild2@sheffield.ac.uk>
2019-07-17 11:08:08 +0100

    Beta version working

--- notipy.py
+++ notipy.py
@@ -8,20 +8,23 @@ import yagmail as yag
 if __name__ == "__main__":
     # Gather the files
     myLoc = '.'
-    fnames = list(glob.iglob('{}/**/*.png'.format(myLoc), recursive=True))
+    fnames = list(glob.iglob('{}/Final_figs/*.pdf'.format(myLoc), recursive=True))
     fnames = [name for name in fnames if not "corner" in name.lower()]
     print("fnames: ")
     for name in fnames:
         print("-> '{}'".format(name))
 
     # Who from, who to
-    send_to = 'wild.james343@gmail.com'
-    send_from = "jwild2@sheffield.ac.uk"
+    send_to = 'jwild2@sheffield.ac.uk'
+
+    with open("email_details.json", 'r') as f:
+        details = json.load(f)
+        send_from = details['user']
+        pword = details['pass']
 
     # Server
     server = "smtp.gmail.com"
 
-    pword = getpass.getpass("Enter email password: ")
 
     subject = "Incoming files"
 
